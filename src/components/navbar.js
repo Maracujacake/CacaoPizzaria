b2156a8cfc3d4674366226274c888f884de5a731
Christian Polli <74564883+Maracujacake@users.noreply.github.com>
2022-08-17 18:53:43 -0300

    Add files via upload

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -20,14 +20,14 @@ function Navbar() {
         <div className='leftSide' id={openLinks ? "open" : "close"}>
           <img src={Logo}/>
           <div className='hiddenLinks'>
-            <Link to="/"> Home </Link>
+            <Link to="/CacauPizzaria"> Home </Link>
             <Link to="/menu"> Menu </Link>
             <Link to="/about"> About </Link>
             <Link to="/contact"> Contact </Link>
           </div>
         </div>
         <div className='rightSide'>
-          <Link to="/"> Home </Link>
+          <Link to="/CacauPizzaria/"> Home </Link>
           <Link to="/menu"> Menu </Link>
           <Link to="/about"> About </Link>
           <Link to="/contact"> Contact </Link>
